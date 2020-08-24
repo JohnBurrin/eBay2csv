fd316b6925c5115a8f5beb9fb1d5a53a4579613b
John <john@synergitech.co.uk>
2020-08-24 10:56:24 +0100

    latest run

--- eBay2csv.sh
+++ eBay2csv.sh
@@ -26,12 +26,14 @@ if [ ! -f "$xslTemplate" ]; then
     exit
 fi
 
-if [ $# > 1 ]; then 
+if [ $# > 1 ]; then
     searchTerms=$2
     locationString=""
     echo "Search terms: $searchTerms"
     # Replace white spaces, this should really be an html entities replace really
     searchTerms="${searchTerms// /%20}"
+    echo "Search terms: $searchTerms"
+
     if [ "$sortOrder" == "DistanceNearest" ]; then
         if [ -z "$buyerPostalCode" ]; then
             echo "Sorting by Distance Nearest requires buyerPostalCode to be set in the config"
@@ -41,43 +43,56 @@ if [ $# > 1 ]; then
             locationString="&buyerPostalCode=$buyerPostalCode"
         fi
     fi
-    
+
     # Replace the spaces in the PostalCode with html entity %20
     buyerPostalCode="${buyerPostalCode// /%20}"
-    
+
     # Download the fist page and extract some data from it.
     echo "Fetching initial information from eBay..."
-    curl -s "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&GLOBAL-ID=$GlobalId&SECURITY-APPNAME=$ebayAppKey&keywords=$searchTerms&itemFilter(0).name=ListingType&itemFilter(0).value=FixedPrice&paginationInput.entriesPerPage=$entriesPerPage&paginationInput.pageNumber=1&sortOrder=$sortOrder&RESPONSE-DATA-FORMAT=XML$locationString" > temp1.xml;
- 
-    # Had some problems with XML name spacing, the quickest workaround for me was to remove the xmlns definition 
-    sed -i.bak 's/xmlns="http:\/\/www.ebay.com\/marketplace\/search\/v1\/services"//g' temp1.xml;
+    echo "https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&GLOBAL-ID=$GlobalId&SECURITY-APPNAME=$ebayAppKey&RESPONSE-DATA-FORMAT=XML$locationString&REST-PAYLOAD=true&itemFilter(0).name=ListingType&itemFilter(0).value=FixedPrice&paginationInput.entriesPerPage=$entriesPerPage&paginationInput.pageNumber=1&sortOrder=$sortOrder&keywords=$searchTerms"
+    curl -s "https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&GLOBAL-ID=$GlobalId&SECURITY-APPNAME=$ebayAppKey&RESPONSE-DATA-FORMAT=XML$locationString&REST-PAYLOAD=true&itemFilter(0).name=ListingType&itemFilter(0).value=FixedPrice&paginationInput.entriesPerPage=$entriesPerPage&paginationInput.pageNumber=1&sortOrder=$sortOrder&keywords=$searchTerms"  > temp.xml;
 
+    # Had some problems with XML name spacing, the quickest workaround for me was to remove the xmlns definition
+    sed -i.bak 's/xmlns="http:\/\/www.ebay.com\/marketplace\/search\/v1\/services"//g' temp.xml;
+
+    # Checking that the xslt file is available
+    if [ ! -f temp.xml ]; then
+        echo "No results were saved in the initial xml file"
+        exit
+    fi
+
+    # Checking that the xslt file is available
+    if [ ! -s temp.xml ]; then
+        echo "No results were saved in the initial xml file - file is empty"
+        exit
+    fi
     # Extract the number of pages in the result set
-    totalPages=`xmllint --xpath '/findItemsAdvancedResponse/paginationOutput/totalPages/text()' temp1.xml`
-    
+    totalPages=`xmllint --xpath '/findItemsAdvancedResponse/paginationOutput/totalPages/text()' temp.xml`
+
     # Extract the total number of entries return by the search
-    totalEntries=`xmllint --xpath '/findItemsAdvancedResponse/paginationOutput/totalEntries/text()' temp1.xml`
-    if [ $totalEntries = 0 ]; then
+    totalEntries=`xmllint --xpath '/findItemsAdvancedResponse/paginationOutput/totalEntries/text()' temp.xml`
+
+    if [[ $totalEntries == 0 ]]; then
         echo "No results found"
         exit
     fi
     echo "Total number of items found $totalEntries in $totalPages pages"
-    
+
     # Remove old file if present
     if [ -f $1 ]; then
         echo "Remove old csv file..."
         rm $1
     fi
 
-    # Create the new CSV file
-    echo "Creating new CSV file..."
-    cat header.csv > $1
+    # Create the new file
+    echo "Creating new file..."
+    #cat header.csv > $1
 
-    echo "Fetching data from eBay..." 
-    for i in $(seq 1 $totalPages); 
-	    do curl -s "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&GLOBAL-ID=$GlobalId&SECURITY-APPNAME=$ebayAppKey&keywords=$searchTerms&itemFilter(0).name=ListingType&itemFilter(0).value=FixedPrice&outputSelector=SellerInfo&paginationInput.entriesPerPage=$entriesPerPage&paginationInput.pageNumber=$i&sortOrder=$sortOrder&RESPONSE-DATA-FORMAT=XML$locationString" > temp$i.xml;
+    echo "Fetching data from eBay..."
+    for i in $(seq 1 $totalPages);
+	    do curl -s "https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&GLOBAL-ID=$GlobalId&SECURITY-APPNAME=$ebayAppKey&RESPONSE-DATA-FORMAT=XML$locationString&REST-PAYLOAD=true&itemFilter(0).name=ListingType&itemFilter(0).value=FixedPrice&paginationInput.entriesPerPage=$entriesPerPage&paginationInput.pageNumber=$i&outputSelector=SellerInfo&sortOrder=$sortOrder&keywords=$searchTerms" > temp$i.xml;
 	    echo "Fetching data from page $i"
-	    
+
         # Had some problems with XML name spacing, the quickest workaround for me was to remove the xmlns definition
         sed -i.bak 's/xmlns="http:\/\/www.ebay.com\/marketplace\/search\/v1\/services"//g' temp$i.xml;
 
@@ -101,4 +116,3 @@ else
 	echo "usage:"
 	echo "get-listings.sh filename.csv \"Some Search Terms\" [config-file.cfg]"
 fi
-
